294e8e18d67bd5754c7b645c23a49a71e1650b6c
Milind Mishra <milind.mishra4@gmail.com>
2021-12-13 02:50:59 +0530

    added initial app

--- app/src/main/java/com/example/birthdaygreetings/MainActivity.kt
+++ app/src/main/java/com/example/birthdaygreetings/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.birthdaygreetings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun createBirthdayCard(view: View) {
+        Toast.makeText(this, "So, you clicked, ay? 🙄", Toast.LENGTH_LONG).show()
     }
 }
